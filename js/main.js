745636261a803fb76c684a74d59ca4918cf0f77a
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2021-11-06 20:47:37 +1100

    Player spawns

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
+import * as Player from "./player.js";
+
 
 //Get the game canvas.
-const canvas = document.getElementById("gameView");
-const gameCanvas = canvas.getContext("2d");
+let canvas = document.getElementById("gameView");
+let gameCanvas = canvas.getContext("2d");
 
 //How many grid blocks per row and column.
 const gridSize = 15;
@@ -9,20 +11,21 @@ const gridSize = 15;
 //Size of each grid block.
 const gridBlockSize = canvas.scrollWidth / gridSize;
 
+const startX = Math.floor((Math.random() * (canvas.scrollWidth - gridBlockSize)) + 1);
+const startY = Math.floor((Math.random() * (canvas.scrollHeight - gridBlockSize)) + 1);;
+const tailLength = 3;
 
+const player = new Player.createPlayer(startX, startY, tailLength);
 
 //Draw grid.
-function drawGrid (gridSize) {
-    for (let row = 0; row < gridSize; row++) {
-        for (let column = 0; column < gridSize; column++) {
-            
-            
-        }
-        
-    }
+function drawGame () {
+    
+    gameCanvas.fillStyle = "red";
+    console.log(canvas.scrollHeight);
+    console.log(gridBlockSize);
+    gameCanvas.fillRect(player.posX, player.posY, gridBlockSize, gridBlockSize);
+    
 }
-
-
 
 //Detect if player is out of bounds.
 
@@ -30,5 +33,11 @@ function drawGrid (gridSize) {
 //Run the game.
 
 function run() {
-    drawGrid(gridSize);
+    drawGame(gridSize);
+    console.log(player.posX);
+    console.log(player.posY);
+    Player.foodCollision();
+    
 }
+
+run();
--- js/player.js
+++ js/player.js
@@ -1,19 +1,27 @@
+import * as Main from "./main.js";
+
 //Snake values.
 let playerPosX;
 let playerPosY;
 
 let tailLength;
 
+export const player = {
+    playerPosX: 0,
+    playerPosY: 0,
 
-//Draw snake.
-function drawPlayer() {
-
+    tailLength: 0,
 }
 
+export function createPlayer(posX, posY, tailLength) {
+    this.posX = posX;
+    this.posY = posY;
+    this.tailLength = tailLength;
+}
 
 //Check for collision with food.
-function foodCollision() {
-
+export function foodCollision() {
+    console.log("Check for food");
 }
 
 //Move snake.
