48e1cc60adf06105bc0f5430a84eb56ab7c88a7f
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2021-11-06 22:11:47 +1100

    player can move but leaves trail

--- js/main.js
+++ js/main.js
@@ -16,7 +16,9 @@ let startX = Math.floor((Math.random() * (canvas.scrollWidth - gridBlockSize)) +
 let startY = Math.floor((Math.random() * (canvas.scrollHeight - gridBlockSize)) + 1);
 const tailLength = 3;
 
+const speed = 15;
 const player = new Player.createPlayer(startX, startY, tailLength);
+
 
 startX = Math.floor((Math.random() * (canvas.scrollWidth - gridBlockSize)) + 1);
 startY = Math.floor((Math.random() * (canvas.scrollHeight - gridBlockSize)) + 1);
@@ -36,17 +38,72 @@ function drawGame () {
     gameCanvas.fillRect(food.posX, food.posY, gridBlockSize, gridBlockSize);
 }
 
+//Move player.
+function movePlayer() {
+    var interval = window.setInterval(function() {
+
+        if (player.velocityX != 0) {
+            player.posX = player.posX + (player.velocityX * speed); 
+        }   
+        
+        if (player.velocityY != 0) {
+            player.posY = player.posY + (player.velocityY * speed); 
+        } 
+
+        gameCanvas.fillStyle = "red";
+        gameCanvas.fillRect(player.posX, player.posY, gridBlockSize, gridBlockSize);
+
+    }, 100);
+}
+
+
+//Detect key press.
+
+document.body.addEventListener('keydown', keyPressed);
+
+
+
+
+function keyPressed(event) {
+        //Pressed left.
+        if (event.keyCode == 37 || event.keyCode == 65) {
+            Player.moveLeft(player);
+        }
+    
+        //Pressed right.
+        if (event.keyCode == 39 || event.keyCode == 68) {
+            Player.moveRight(player);
+        }
+    
+        //Pressed up.
+        if (event.keyCode == 38 || event.keyCode == 87 && Player.playerVelocityY != 1) {
+            Player.moveUp(player);
+        }
+    
+        //Pressed down.
+        if (event.keyCode == 40 || event.keyCode == 83) {
+            Player.moveDown(player);
+        }
+}
+
+
 //Detect if player is out of bounds.
 
 
 //Run the game.
 
 function run() {
-    drawGame(gridSize);
-    console.log(player.posX);
-    console.log(player.posY);
+
+
+    movePlayer();
+    drawGame();
     Player.foodCollision();
     
+
 }
 
 run();
+
+document.getElementById("btnUp").addEventListener("click", () => {
+    alert("Retard");
+});
--- js/player.js
+++ js/player.js
@@ -2,6 +2,12 @@ export function createPlayer(posX, posY, tailLength) {
     this.posX = posX;
     this.posY = posY;
     this.tailLength = tailLength;
+
+    this.velocityX = 0;
+    this.velocityY = 0;
+    this.isAlive = true;
+
+    
 }
 
 //Check for collision with food.
@@ -9,5 +15,44 @@ export function foodCollision() {
     console.log("Check for food");
 }
 
-//Move snake.
+export function moveLeft(player) {
+    console.log("Move left.");
+
+    //Make sure player can't 
+    //go opposite direction
+    if (player.velocityX != 1) {
+        player.velocityX = -1;
+        player.velocityY = 0;
+    }
+}
+
+export function moveRight(player) {
+    console.log("Move right.");
+
+    //Make sure player can't 
+    //go opposite direction
+    if (player.velocityX != -1) {
+        player.velocityX = 1;
+        player.velocityY = 0;
+    }
+
+
+}
+
+export function moveUp(player) {
+    console.log("Move up.");
+
+    if (player.velocityY != 1) {
+        player.velocityX = 0;
+        player.velocityY = -1;
+    }
+}
+
+export function moveDown(player) {
+    console.log("Move down.");
+    if (player.velocityY != -1) {
+        player.velocityX = 0;
+        player.velocityY = 1;
+    }
+}
 
